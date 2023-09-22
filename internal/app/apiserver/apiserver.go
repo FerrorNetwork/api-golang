2d36c872a688ce4fb7399495a4845ee7b4e796c5
FerrorNetwork <40063558+FerrorNetwork@users.noreply.github.com>
2023-09-22 14:36:17 +0400

    implement register user

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,7 +1,9 @@
 package apiserver
 
 import (
+	"api-golang/internal/app/model"
 	"api-golang/internal/app/store"
+	"encoding/json"
 	"io"
 	"net/http"
 
@@ -75,7 +77,41 @@ func (s *Apiserver) handleHello() http.HandlerFunc {
 }
 
 func (s *Apiserver) handleUsersCreate() http.HandlerFunc {
+	type request struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
 	return func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Users")
+		// ...
+		req := &request{}
+		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+			s.error(w, r, http.StatusBadRequest, err)
+			return
+		}
+
+		u := &model.User{
+			Email:    req.Email,
+			Password: req.Password,
+		}
+		if err := s.store.User().Create(u); err != nil {
+			s.error(w, r, http.StatusUnprocessableEntity, err)
+			return
+		}
+
+		u.Sanitize()
+		s.respond(w, r, http.StatusCreated, u)
 	}
 }
+
+func (s *Apiserver) error(w http.ResponseWriter, r *http.Request, code int, err error) {
+	s.respond(w, r, code, map[string]string{"error": err.Error()})
+}
+
+func (s *Apiserver) respond(w http.ResponseWriter, r *http.Request, code int, data interface{}) {
+	w.WriteHeader(code)
+	if data != nil {
+		json.NewEncoder(w).Encode(data)
+	}
+}
+
